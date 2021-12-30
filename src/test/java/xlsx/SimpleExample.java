ed78c5f9808a559a73bdbaa1e3bdc49a125716fa
DaniilsLoputevs <55362320+DaniilsLoputevs@users.noreply.github.com>
2021-12-30 22:35:21 +0000

    todos

--- src/test/java/xlsx/SimpleExample.java
+++ src/test/java/xlsx/SimpleExample.java
@@ -2,6 +2,12 @@ package xlsx;
 
 import org.junit.Test;
 
+/**
+ * TODO : support CompletableFuture<Iterable<T>> in income data, not only Iterable<T>.
+ * TODO : nextSheet() && nextBlock()
+ * TODO : ? native cells ?
+ * TODO : remove Generic from CellGroupSelector
+ */
 public class SimpleExample {
     @Test
     public void name() {
